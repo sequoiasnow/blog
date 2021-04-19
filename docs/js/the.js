afd79577d74afc75434f8da00debd86af68426cc
sequoiasnow <the1codemaster@gmail.com>
2021-04-19 14:52:46 -0700

    Publish.

---
+++ docs/js/the.js
@@ -0,0 +1,29 @@
+function setColorTheme(theme) {
+  if (theme == 'dark') {
+    document.body.classList.add('dark');
+  } else {
+    document.body.classList.remove('dark');
+  }
+
+  // Swap the button text
+  let other = (theme == 'dark') ? 'Light' : 'Dark';
+  document.getElementsByClassName('dark-mode')[0].innerHTML = other + ' Mode';
+
+  // Persist the theme
+  window.localStorage.setItem('color-theme', theme);
+}
+
+
+function swapDarkLight() {
+  if (document.body.classList.contains('dark')) {
+    setColorTheme('light');
+  } else {
+    setColorTheme('dark');
+  }
+}
+
+// Restore theme from session
+setColorTheme(window.localStorage.getItem('color-theme'));
+
+// JS might not be enabled, if so we might want to hide the toggle.
+document.body.classList.add('js');
